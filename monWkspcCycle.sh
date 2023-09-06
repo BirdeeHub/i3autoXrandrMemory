c649759583a7ac2076066d40c2907320fde3a29e
archibird <robindbor@gmail.com>
2023-09-06 03:45:27 -0700

    Whoops, bug in new script

--- monWkspcCycle.sh
+++ monWkspcCycle.sh
@@ -6,9 +6,9 @@
 currentWkspc="$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).num')"
 currentMon="$(i3-msg -t get_outputs | jq -r ".[] | select(.current_workspace == \"$currentWkspc\") | .name")"
 readarray -t activeMons <<< "$(xrandr --listactivemonitors | awk '{print($4)}' | tail -n +2)"
-activeMons+=( "${activeMons[@]}" )
+activeMons+=( "${activeMons[0]}" )
 found="false"
 for mon in "${activeMons[@]}"; do
     [[ "$found" == "true" ]] && exec i3-msg "workspace number $currentWkspc, move workspace to output $mon"
-    [[ "$mon" == "$currentMon" ]] && found="true" && bash -c "i3-msg \"workspace $mon\""
+    [[ "$mon" == "$currentMon" ]] && found="true" && bash -c "i3-msg \"workspace $currentWkspc\""
 done
